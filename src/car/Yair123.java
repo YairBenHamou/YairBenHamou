380d392d407c6c9718de4b6d7738866bb4e8511c
Eti Ben Hamo <Eti Ben Hamo@10.0.0.11>
2022-06-07 18:59:57 +0300

    hellow world

---
+++ src/car/Yair123.java
@@ -0,0 +1,19 @@
+package car;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Yair123 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Eti Ben Hamo\\Desktop//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	
+		
+				
+				
+				
+	}
+	
+
+}
